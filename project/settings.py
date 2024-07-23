de45303c449da315e657ae160c25a0eb9af952b0
ahmed yasser <a.yasser@kmy.local>
2024-07-23 17:08:09 +0300

    add job models

--- project/settings.py
+++ project/settings.py
@@ -121,8 +121,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATICFILES_DIRS = ("/static")
 
-STATIC_URL = 'static/'
+STATIC_URL = STATICFILES_DIRS
+
+STATICFILES_DIRS = [
+    BASE_DIR /'static',
+    '/var/www/static/',
+]
+
+#MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
