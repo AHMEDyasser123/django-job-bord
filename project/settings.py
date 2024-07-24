72e9b25fcee6847ffa317102c55bbc013ad1bebb
ahmed yasser <a.yasser@kmy.local>
2024-07-24 14:37:22 +0300

    add job models

--- project/settings.py
+++ project/settings.py
@@ -121,13 +121,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATICFILES_DIRS = ("/static")
 
-STATIC_URL = STATICFILES_DIRS
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     BASE_DIR /'static',
-    '/var/www/static/',
+    
 ]
 
 #MEDIA_URL = 'media/'
